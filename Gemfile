edf53288696039e68e1da62e92c916d8bf2017dc
Erik Dungan <info@2tablespoons.com>
2011-11-17 22:01:39 -0800

    First

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.2'
+gem 'mysql2'
+gem 'stripe', :git => 'https://github.com/stripe/stripe-ruby'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5.rc.2'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', '0.8.2', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thylacine::Application.config.secret_token = '19bcbdfa20462593dc03475589863858d8829f8326bb7a82920dfc7cc4ba3534242ba1b2c33f7080d8edb3df370022a4fef1e47411b9c062ccd2b5cecdeea712'
